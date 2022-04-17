6ee3d6081324266b82601d02d0d69901b9af6bed
Caster <daniel.dabrowski.177@gmail.com>
2022-04-17 19:43:09 +0200

    new branch - home

--- scripts/test.js
+++ scripts/test.js
@@ -1,1 +1,12 @@
+let banner = document.querySelector(".banner")
+let sw = document.querySelector(".swiper")
+
 console.log("works")
+// banner.addEventListener("click", () => {
+
+// })
+
+sw.style.height = "135px"
+banner.addEventListener("click", () => {
+  sw.style.height = "250px"
+})
