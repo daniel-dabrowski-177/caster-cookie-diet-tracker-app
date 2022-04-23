15ae7af099875307ffe5314caa327c0587fab05d
Caster <daniel.dabrowski.177@gmail.com>
2022-04-23 17:04:46 +0200

    'backpack-changes'

--- scripts/products.js
+++ scripts/products.js
@@ -1,4 +1,4 @@
-console.log(products)
+// console.log(products)
 
 const container = document.querySelector(".container")
 
@@ -15,3 +15,19 @@ for (let i = 0; i < 80; i++) {
   div.classList.add("product")
   container.appendChild(div)
 }
+
+const product = document.querySelectorAll(".product")
+const clicked = document.querySelectorAll(".clicked")
+
+product.forEach((p) =>
+  p.addEventListener("click", () => {
+    // console.log(p.classList[0])
+
+    if (p.classList[1] == null) {
+      p.classList.add("clicked")
+    } else {
+      p.classList.remove("clicked")
+    }
+    console.log(product[4])
+  })
+)
