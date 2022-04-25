80f83d6217964352481a4f5953956acafd97ce0d
Caster <daniel.dabrowski.177@gmail.com>
2022-04-25 10:18:47 +0200

    'working_local_storage'

--- scripts/products.js
+++ scripts/products.js
@@ -1,8 +1,22 @@
 const container = document.querySelector(".container")
+const home = document.querySelector(".icon-center")
 let count = 0
+
+const localStorageContent = localStorage.getItem("names")
+let names
+
+if (localStorageContent === null) {
+  names = [""]
+} else {
+  names = JSON.parse(localStorageContent)
+}
 
 for (let i = 0; i < 80; i++) {
   let div = document.createElement("div")
+
+  if (names.includes(products[i].name, 1)) {
+    div.classList.add("clicked")
+  }
 
   div.innerHTML = `<div class="bg-icon">
   <img src="/img/icons/${products[i].name}.png" alt="" />
@@ -16,42 +30,22 @@ for (let i = 0; i < 80; i++) {
 const product = document.querySelectorAll(".product")
 const clicked = document.querySelectorAll(".clicked")
 
-const localStorageContent = localStorage.getItem("names")
-
-let names
-if (localStorageContent === null) {
-  names = []
-} else {
-  names = JSON.parse(localStorageContent)
-}
-
 product.forEach((p) =>
   p.addEventListener("click", () => {
     let productName = p.children[1].textContent
+    console.log("sth")
 
     if (p.classList[1] == null) {
-      p.classList.add("clicked")
       count++
-
+      p.classList.add("clicked")
       names.push(productName)
       localStorage.setItem("names", JSON.stringify(names))
     } else {
       p.classList.remove("clicked")
       count--
-
       let pos = names.indexOf(productName)
-      names.splice(pos, 999)
+      names.splice(pos, 1)
       localStorage.setItem("names", JSON.stringify(names))
     }
   })
 )
-console.log(names)
-
-// console.log(product[2].children[1].textContent)
-
-// console.log(localStorage)
-// console.log(clicked.parentNode)
-// console.log(container.childNodes[2])
-
-// Product name //
-// console.log(container.childNodes[2].children[1].innerHTML)
